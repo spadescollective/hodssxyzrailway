f8f002741966af4b238a7bfac4213bebfa7cc607
hod <98139763+hodss@users.noreply.github.com>
2022-03-31 20:26:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+//require express framework
+const express = require('express');
+//express function
+const app = express();
+//Deprecation of res.sendfile is invalid, "sendFile" is currently not working, so ignore the console log, as it's not true.
+
+// GET / file/index landing
+app.get('/', (req, res) => {
+  res.sendfile('src/views/pages/landing.htm')
+});
+// GET/SEND file
+app.get('/second', (req, res) => {
+  res.sendfile('pages/secondpage.html')
+});
+
+// Call Public Static
+app.use('/public', express.static(process.cwd() + '/public'));
+
+// Handle 404
+app.use(function(req, res) {
+  res.sendfile('errors/404.htm', 404);
+});
+
+// Handle 500
+app.use(function(error, req, res, next) {
+  res.sendfile('errors/500.htm', 500);
+});
+//listen on ports and give serverstart and serverready
+app.listen(3000, () => {
+  console.log('Server Ready');
+  console.log('Server Start');
+  console.log('Ignore deprecation messages below this one');
+  
+});
